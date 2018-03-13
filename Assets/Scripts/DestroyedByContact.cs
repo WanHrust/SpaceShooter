c8cdb33c1bf5daca9d14108439d6ccfaac8338d2
Orhan <orhanlock@gmail.com>
2018-03-13 14:52:12 +0400

    Lesson 2.4 GameController
    Added explosions to player and to asteriods.

--- Assets/Scripts/DestroyedByContact.cs
+++ Assets/Scripts/DestroyedByContact.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class DestroyedByContact : MonoBehaviour
 {
+    public GameObject playerExplosion;
+    public GameObject explosion;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
         {
             return;
+        }
+        Instantiate(playerExplosion, transform.position, transform.rotation); // as GameObject;
+        if (other.tag == "player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation); // as GameObject;
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject hazard;
+    public Vector3 spawnValue;
+
+    void Start()
+    {
+        SpawnWaves();
+    }
+    void SpawnWaves()
+    {
+        Quaternion spawnRotation = new Quaternion.identity (Random.Range (-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+        Vector3 spawnPosition = new Vector3 ();
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
